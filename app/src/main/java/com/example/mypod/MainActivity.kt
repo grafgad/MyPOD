3743e2f5c7f129239ed61cebfd4a9249b6093459
grafgad <grafgad@mail.ru>
2021-07-10 22:28:34 +0300

    Made bottom sheet.

---
+++ app/src/main/java/com/example/mypod/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mypod
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
