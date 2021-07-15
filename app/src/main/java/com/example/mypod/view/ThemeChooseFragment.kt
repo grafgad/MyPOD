1b511358e344f74ae711d1079c498710070ce9ab
grafgad <grafgad@mail.ru>
2021-07-16 00:44:42 +0300

    Theme works

--- app/src/main/java/com/example/mypod/view/ThemeChooseFragment.kt
+++ app/src/main/java/com/example/mypod/view/ThemeChooseFragment.kt
@@ -4,22 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import com.example.mypod.R
 import kotlinx.android.synthetic.main.theme_choose_fragment.*
 
 class ThemeChooseFragment : Fragment() {
-
-    private val themedContext = ContextThemeWrapper(
-        context,//контекст вашей Активити
-        R.style.IndigoTheme //ваш стиль из ресурсов
-    )
-
-
-    //любой вью, который вы хотите создать через конструктор или надуть через Inflator
- //   val view = MyCustomView(themedContext)
-//    val otherView = LayoutInflater.from(themedContext).inflate(R.layout.main_fragment, container, false)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,35 +20,30 @@ class ThemeChooseFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        cosmos_theme_button.setOnClickListener(){
-            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.container, PictureOfTheDayFragment())?.addToBackStack(null)?.commit()
+        cosmos_theme_button.setOnClickListener {
+            activity?.setTheme(R.style.CosmosTheme)
+            getMainFragment()
         }
-        moon_theme_button.setOnClickListener(){
-            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.container, PictureOfTheDayFragment())?.addToBackStack(null)?.commit()
+        moon_theme_button.setOnClickListener {
+            activity?.setTheme(R.style.MoonTHeme)
+            getMainFragment()
         }
-        mars_theme_button.setOnClickListener(){
-            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.container, PictureOfTheDayFragment())?.addToBackStack(null)?.commit()
+        mars_theme_button.setOnClickListener {
+            activity?.setTheme(R.style.MarsTheme)
+            getMainFragment()
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private fun getMainFragment (){
+        activity
+            ?.supportFragmentManager
+            ?.beginTransaction()
+            ?.replace(R.id.container, PictureOfTheDayFragment())
+            ?.addToBackStack(null)
+            ?.commit()
     }
-
-
-
-//    private fun somefun(){
-//        R.id.moon_theme_button
-//        cosmos_theme_button.setOnClickListener(){
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, ThemeChooseFragment.newInstance())
-////                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
-//                .commitNow()
-//        }
-//    }
 
     companion object {
         fun newInstance() = ThemeChooseFragment()
-        private var isMain = true
     }
 }
